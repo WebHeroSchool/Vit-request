cbab92181dde3a10642262c00b1e48871e77e516
WhitcherX-PC <kot_X@mail.ru>
2019-08-11 14:18:03 +1000

    make task2

--- script.js
+++ script.js
@@ -1,50 +1,79 @@
 let body = document.body;
-let url = window.location.toString();
+let avatar;
+let name;
+let description;
+let url;
+let savedDate;
+let profileName;
 
-let getName = (url) => {
-	let urlMas = url.split('=');
-	let name = urlMas[1];	
-	if(name == undefined) {
-		name = 'WhitcherX';
- 	}
- 	return name;
+let getName = new Promise((resolve, reject) => {
+	setTimeout(() => {
+		let url = window.location.toString();
+		let urlMas = url.split('=');
+		let name = urlMas[1];	
+		resolve(name == 'undefined' ? name : 'WhitcherX');
+	}, 3000);
+});
+
+let getDate = new Promise((resolve, reject) => {
+	setTimeout(() => {
+		let date = new Date();
+		resolve(date.getDate() + '.' + (date.getMonth() + 1) + '.' + date.getFullYear());
+	}, 2000);
+});
+
+let initializeVar = (json) => {
+	avatar = json.avatar_url;
+	name = json.name;
+	description = json.bio;
+	url = json.url;
+}
+let addName = () => {
+	let userName = document.createElement('h1');
+	userName.innerHTML = name;
+	body.appendChild(userName);
+}
+let addDescription = () => {
+	let userDescription = document.createElement('h2');
+	userDescription.innerHTML = description;
+	body.appendChild(userDescription);
+}
+let addAvatar = () => {
+	let userAvatar = document.createElement('img');
+	let newString = document.createElement('br');
+	userAvatar.src = avatar;
+	body.appendChild(userAvatar);
+	body.appendChild(newString);
+}
+let addUrl = () => {
+	let userUrl = document.createElement('a');
+	let text = document.createTextNode('profile');
+	userUrl.appendChild(text);
+	userUrl.href = 'https://github.com/' + profileName;
+	body.appendChild(userUrl);
+}
+let addDate = () => {
+	let currentDate = document.createElement('h2');
+	currentDate.innerHTML = savedDate;
+	body.appendChild(currentDate);
+}
+let hidePreload = () => {
+	let preloader = document.getElementById('container');
+	preloader.style.display = 'none';
 }
 
-let name = getName(url);
-console.log('https://api.github.com/users/' + name);
-fetch('https://api.github.com/users/' + name)
+Promise.all([getName, getDate])
+	.then(([name, date]) => {
+		savedDate = date;
+		profileName = name;
+		return fetch('https://api.github.com/users/' + name);})
 	.then(res => res.json())
 	.then(json => {
-		avatar = json.avatar_url;
-		userName = json.name;
-		description = json.bio;
-		url = json.url;
-		let addName = () => {
-			let userName = document.createElement('h1');
-			userName.innerHTML = name;
-			body.appendChild(userName);
-		}
-		let addDescription = () => {
-			let userDescription = document.createElement('h2');
-			userDescription.innerHTML = description;
-			body.appendChild(userDescription);
-		}
-		let addAvatar = () => {
-			let userAvatar = document.createElement('img');
-			let newString = document.createElement('br');
-			userAvatar.src = avatar;
-			body.appendChild(userAvatar);
-			body.appendChild(newString);
-		}
-		let addUrl = () => {
-			let userUrl = document.createElement('a');
-			let text = document.createTextNode('profile');
-			userUrl.appendChild(text);
-			userUrl.href = 'https://github.com/' + name;
-			body.appendChild(userUrl);
-		}
+		initializeVar(json);
+		hidePreload();
 		addName();
 		addDescription();
 		addAvatar();
 		addUrl();
-	}).catch(err => alert(err + 'Информация о пользователе не доступна'));
+		addDate();}
+	).catch(err => alert(err + 'Информация о пользователе не доступна'));
